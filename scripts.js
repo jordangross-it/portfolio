c96211e3c780b8ab0d1210dd8135c870efea2299
Jordan Gross <grossjordan07@gmail.com>
2026-01-27 17:03:21 -0500

    updated scripts

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
-console.log("Back-to-top script loaded");
+// Grab the hamburger icon and nav links
+const hamburger = document.getElementById("hamburger-icon");
+const navLinks = document.getElementById("nav-links");
+
+// Toggle 'show' class when hamburger is clicked
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// Close mobile menu when a link is clicked
+document.querySelectorAll("#nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navLinks.classList.contains("show")) {
+      navLinks.classList.remove("show");
+    }
+  });
+});
 
 // get button
-      const backToTopButton = document.getElementById("backToTop");
+const backToTopButton = document.getElementById("backToTop");
 
-      // show button when user scrolls center length down
-      window.onscroll = function () {
-        if (
-          document.body.scrollTop > 100 ||
-          document.documentElement.scrollTop > 100
-        ) {
-          backToTopButton.style.display = "block";
-        } else {
-          backToTopButton.style.display = "none";
-        }
-      };
+// show button when user scrolls center length down
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
 
-      // scroll to top when button clicked
-      backToTopButton.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      });
+// scroll to top when button clicked
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
